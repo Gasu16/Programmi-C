2e1b81519f88c5fbf96666fd98451ffc981d974f
Matteo Grammatico <kizaru774@gmail.com>
2018-05-15 16:53:53 +0200

    Add files via upload

---
+++ leggiFile.c
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   mainFile.c
+ * Author: matteo
+ *
+ * Created on 15 maggio 2018, 15.45
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    FILE *fp;
+    char testo [100];
+    char line [80];
+    fp = fopen("/home/matteo/NetBeansProjects/GestioneFile/filen.txt", "r");
+    /* Cambia il path di fopen a seconda del file che vuoi leggere */
+    if(fp == NULL){
+        printf("File non trovato");
+    }
+    while(fscanf(fp, "%s", &testo) != EOF){
+        /* Quello che andiamo a leggere da file lo metto in "testo" 
+         fin quando arrivo a EOF (end of file) */
+        printf(" %s", testo);
+    }
+    fclose(fp);
+    return (EXIT_SUCCESS);
+}
+
