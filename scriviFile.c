1e517486617434115cee31846327fac82459e232
Matteo Grammatico <kizaru774@gmail.com>
2018-05-21 15:55:37 +0200

    Add files via upload

---
+++ scriviFile.c
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   scriviFile.c
+ * Author: matteo
+ *
+ * Created on 15 maggio 2018, 15.45
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#define DIM 256
+
+int main(int argc, char** argv) {
+    FILE *fp;
+    char testo [DIM];  
+    char stringa [DIM];
+    fp = fopen("/home/matteo/NetBeansProjects/GestioneFile/filen.txt", "w+");
+
+    /* Cambia il path di fopen a seconda del file che vuoi leggere */
+    if(fp == NULL){
+        printf("File non trovato");
+        exit(EXIT_FAILURE); // System call
+    }
+    printf("Che cosa vuoi scrivere: \n");
+    fgets(stringa, DIM, stdin);
+    fprintf(fp, stringa); // Scriviamo la stringa sul file indicato da fp
+    fseek(fp, 0, SEEK_SET); // Resettiamo il puntatore a 0 per leggere il file dall'inizio
+    while(fscanf(fp, "%s", &testo) != EOF){
+        /* Quello che andiamo a leggere da file lo metto in "testo" 
+         fin quando arrivo a EOF (end of file) */
+        printf(" %s", testo);
+    }
+    
+    /* Dimensione del file */
+    int length = ftell(fp);
+    printf("\nDimensione file: %d byte", length);
+    fclose(fp);
+    return 0;
+}
+
